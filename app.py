574f05ab3e2279eccbc5311d24fbaa8076316532
spacegrowth <spacegrowth@proton.me>
2025-04-10 04:24:15 +0000

    fixing libraries

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+from scipy.stats import norm  # Add this import for the norm function
 
 # Set page configuration
 st.set_page_config(
